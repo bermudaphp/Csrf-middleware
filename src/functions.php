41148439ff61bb0f45b52916e619236bdf008842
Andrey <alestro@bk.ru>
2020-08-25 23:06:58 +0200

    Create functions.php

---
+++ src/functions.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Bermuda\Middleware
+
+
+/**
+ * @return string
+ */
+function csrf_field(): string
+{
+    return '<input type="hidden" name="'. CsrfMiddleware::tokenKey .'" value="'. CsrfMiddleware::getToken() .'">';
+}
+
+/**
+ * @return string
+ */
+function csrf_token(): string
+{
+    return CsrfMiddleware::getToken();
+}
