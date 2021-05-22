c5960dfb2f46ca500da2298b34a0095dfbd4b588
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-05-22 14:41:44 +0300

    Update CsrfMiddleware.php

--- src/CsrfMiddleware.php
+++ src/CsrfMiddleware.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace Bermuda\Middleware;
-
 
 use Dflydev\FigCookies\SetCookie;
 use Dflydev\FigCookies\FigResponseCookies;
@@ -11,7 +9,6 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-
 
 /**
  * Class CsrfMiddleware
